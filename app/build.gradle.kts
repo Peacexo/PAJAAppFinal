571cf852191328c05073ab6cfde19ad02d0b7318
torr0119 <torr0119@algonquinlive.com>
2024-04-03 19:34:35 -0400

    Final Project - Favorite Class and Database implemented final.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.android.volley:volley:1.2.1")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0") //Implemented by Allan
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.recyclerview:recyclerview:1.+")
